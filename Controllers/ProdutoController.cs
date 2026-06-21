2fb3e5b64ad01e9da21845120830af29ce735bba
MANSJR13 <m.aureliosilvajr@outlook.com.br>
2026-06-21 11:04:44 -0300

    Primeiros passos com API

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApiProdutos.Models;
+
+namespace ApiProdutos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private static List<Produto> produtos = new List<Produto>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(produtos);
+        }
+        [HttpPost]
+        public IActionResult Post(Produto produto)
+        {
+            produto.Id = produtos.Count + 1;
+            produtos.Add(produto);
+            return Ok(produto);
+        }
+
+    }
+}
